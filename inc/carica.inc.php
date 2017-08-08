d51868eb55675f72f422a756175cd340366fb8d7
Edoardo Savini <sedoardo98@gmail.com>
2017-08-08 16:38:46 +0200

    Registrate le classi TemplateManager e Dizionario.

--- inc/carica.inc.php
+++ inc/carica.inc.php
@@ -3,7 +3,11 @@
   require_once(__DIR__.'/../class/logs.class.php');
   require_once(__DIR__.'/../class/notifiche.class.php');
   require_once(__DIR__.'/../class/paginator.class.php');
+  require_once(__DIR__.'/../class/dizionario.class.php');
+  require_once(__DIR__.'/../class/templateManager.class.php');
   require_once('mysqli.inc.php');
 
   $console = new Console($mysqli);
+  $dizionario = new Dizionario($mysqli, 'dizionario');
+  $templateManager = new TemplateManager($mysqli, 'templates');
 ?>
