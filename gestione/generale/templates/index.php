4b83dc337242af771e30542666d7c954039655dd
Edoardo Savini <sedoardo98@gmail.com>
2017-08-08 16:35:24 +0200

    Aggiunta gestione dei template La classe TemplateManager permette di gestire vari template e la sostituzione dei segnaposti con dei valori.

---
+++ gestione/generale/templates/index.php
@@ -0,0 +1,58 @@
+<?php
+  require_once('../../../inc/autenticazione.inc.php');
+
+  if($autenticazione -> gestionePortale != 1)
+    header('Location: /');
+?>
+<!DOCTYPE html>
+<html lang="it">
+  <head>
+    <?php
+      require_once('../../../inc/header.inc.php');
+    ?>
+  </head>
+  <body>
+    <?php
+      include_once('../../../inc/nav.inc.php');
+    ?>
+    <div id="contenuto">
+      <h2>Templates</h1>
+      <div style="overflow-x: auto;">
+        <?php
+          $sql = "SELECT * FROM templates";
+
+          if($query = $mysqli -> query($sql)) {
+        ?>
+        <table>
+          <thead>
+            <tr>
+              <th>Titolo</th>
+              <th>Azioni</th>
+            </tr>
+          </thead>
+          <tbody>
+            <?php
+              // Stampo gli utenti
+              while($row = $query -> fetch_assoc()) {
+
+                echo "<tr>";
+                echo "<td>{$row['titolo']}</td>";
+                echo "<td><a href=\"/gestione/generale/templates/modifica.php?id={$row['id']}\">Modifica</a></td>";
+                echo "</tr>";
+              }
+            ?>
+          </tbody>
+        </table>
+        <?php
+          } else {
+            $console -> alert('Impossibile comunicare con il database! '.$mysqli -> error, $autenticazione -> id);
+            echo "<p>Impossibile completare la richiesta!</p>";
+          }
+        ?>
+      </div>
+    </div>
+    <?php
+      include_once('../../../inc/footer.inc.html');
+    ?>
+  </body>
+</html>
