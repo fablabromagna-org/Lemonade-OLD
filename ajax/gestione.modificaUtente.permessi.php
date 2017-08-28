122de403d765eb370dde77a105ba76efc06bd29f
Edoardo Savini <sedoardo98@gmail.com>
2017-08-28 18:15:59 +0200

    Riscrittura completa

--- ajax/gestione.modificaUtente.permessi.php
+++ ajax/gestione.modificaUtente.permessi.php
@@ -19,7 +19,7 @@
   $confermaMail = $mysqli -> real_escape_string(isset($_POST['confermaMail']) ? trim($_POST['confermaMail']) : '');
   $id = $mysqli -> real_escape_string(isset($_POST['id']) ? trim($_POST['id']) : '');
 
-  function stampaErrore($errore = 'Errore sconosciuto!') {
+  function stampaErrore($errore = 'Impossibile completare la richiesta.') {
     echo '{"errore":true,"msg":"'.$errore.'"}';
     exit();
   }
@@ -31,140 +31,138 @@
     stampaErrore('Non hai effettuato l\'accesso!');
 
   // Controllo che l'utente abbia i permessi per effettuare la modifica
-  else if(!$permessi -> whatCanHeDo($autenticazione -> id)['gestioneUtentiAvanzata']['stato'])
+  if(!$permessi -> whatCanHeDo($autenticazione -> id)['gestioneUtentiAvanzata']['stato'])
 
     // L'utente non ha i permessi
     stampaErrore('Non sei autorizzato ad effettuare la modifica!');
 
   // L'utente ha effettuato l'accesso ed è autorizzato
-  else {
+  // Controllo i valori
+  if(!preg_match("/^[0-9]{1,11}+$/i", $id))
+    stampaErrore('ID non valido!');
 
-    // Controllo l'id sia diverso da vuoto
-    if($id == '')
-      stampaErrore('ID mancante!');
+  if(!preg_match("/^[0-9]{1}+$/i", $confermaMail))
+    stampaErrore('Valore non valido!');
 
-    // Controllo che i valori siano validi
-    else if($categoria == '')
-      stampaErrore('Devi inserire una categoria!');
+  if(!preg_match("/^[0-9]{1}+$/i", $sospensione))
+    stampaErrore('Valore non valido!');
 
-    else if($sospensione != '1' && $sospensione != '0')
-      stampaErrore('Devi indicare lo stato di sospensione dell\'account!');
+  if(!preg_match("/^[0-9]{1,11}+$/i", $categoria))
+    stampaErrore('ID categoria non valido!');
 
-    else if($confermaMail != '1' && $confermaMail != '0')
-      stampaErrore('Devi indicare lo stato di verifica dell\'indirizzo email!');
+  // Estraggo i dati dell'utente
+  $sql = "SELECT * FROM utenti WHERE id = {$id} LIMIT 0, 1";
+  $query = $mysqli -> query($sql);
 
-    // Aggiorno i dati nel database
-    else {
-
-      // Estraggo i dati relativi all'utente
-      $sql = "SELECT * FROM utenti WHERE id = '{$id}'";
-
-      if($query = $mysqli -> query($sql)) {
-
-        // Nessun utente è stato trovato
-        if($query -> num_rows != 1)
-          stampaErrore('L\'utente richiesto non è presente nel database.');
-
-        // Controllo se i dati sono stati modificati
-        else {
-
-          $modificati = false;
-          $row = $query -> fetch_assoc();
-
-          if($row['categoria'] != $categoria)
-            $modificati = true;
-
-          if($row['sospeso'] != $sospensione)
-            $modificati = true;
-
-          if($row['codiceAttivazione'] != $confermaMail)
-            $modificati = true;
-
-          // Avviso l'utente che nessun dato è stato modificato
-          if(!$modificati)
-            stampaErrore('Nessun dato è stato modificato.');
-
-          // I dati sono stati modificati
-          // Controllo che la categoria esista
-          $vecchiaRow = $row;
-
-          $sql = "SELECT * FROM categorieUtenti WHERE id = '{$categoria}'";
-
-          if(!$query = $mysqli -> query($sql))
-            stampaErrore('Impossibile inviare la richiesta al database!');
-
-          else {
-
-            // Se il database restituisce più di una riga
-            // E' sicuramente presente un errore
-            if($query -> num_rows > 1)
-              stampaErrore('Database corrotto!');
-
-            // Se restituisce una riga la categoria esiste
-            else if($query -> num_rows == 1) {
-
-              $codiceAttivazione = '0';
-              if($row['codiceAttivazione'] == '0' && $confermaMail == '0')
-                $codiceAttivazione = uniqid();
-
-              $sql = "UPDATE utenti SET categoria = '{$categoria}', sospeso = '{$sospensione}', codiceAttivazione = '{$codiceAttivazione}' WHERE id = '{$id}'";
-
-              if($mysqli -> query($sql)) {
-
-                // Se la conferma dell'indirizzo email è cambiata
-                // Rinvio la mail
-                if($codiceAttivazione != '0') {
-
-                  $linkVerifica = $dizionario -> getValue('urlSito').'/confermaMail.php?token='.$codiceAttivazione;
-
-                  try {
-                    $replyTo = ($dizionario -> getValue('EMAIL_REPLY_TO') == false || $dizionario -> getValue('EMAIL_REPLY_TO') == null) ? $dizionario -> getValue('EMAIL_SOURCE') : $dizionario -> getValue('EMAIL_REPLY_TO');
-                    $html = $templateManager -> getTemplate('confermaMailGestione', array(
-                      'nome' => $nome,
-                      'cognome' => $cognome,
-                      'linkConferma' => $linkVerifica,
-                      'nomeSito' => $dizionario -> getValue('nomeSito')
-                    ));
-
-                    $client -> sendEmail(array(
-                      'Source' => $dizionario -> getValue('EMAIL_SOURCE'),
-                      'ReplyToAddresses' => array($replyTo),
-                      'Destination' => array(
-                        'ToAddresses' => array($row['email'])
-                      ),
-                      'Message' => array(
-                        'Subject' => array(
-                          'Data' => 'Verifica indirizzo email',
-                          'Charset' => 'UTF-8'
-                        ),
-                        'Body' => array(
-                          'Html' => array(
-                            'Data' => $html,
-                            'Charset' => 'UTF-8'
-                          )
-                        )
-                      )
-                    ));
-
-                    echo '{}';
-
-                  } catch(Exception $e) {
-                    stampaErrore('Impossibile inviare l\'email!');
-                  }
-
-                } else
-                  echo '{}';
-
-              } else
-                stampaErrore('Impossibile aggiornare i dati dell\'utente!');
-
-            // Se non restituisce nulla la categoria non esiste
-            } else
-              stampaErrore('Categoria inesistente!');
-          }
-        }
-      } else
-        stampaErrore('Impossibile inviare la richiesta al database!');
-    }
+  if(!$query) {
+    $console -> alert('Impossibile controllare l\'esistenza dell\'utente. '.$mysqli -> error, $autenticazione -> id);
+    stampaErrore();
   }
+
+  // Controllo che l'utente esista
+  if($query -> num_rows === 0)
+    stampaErrore('Utente inesistente!');
+
+  $utente = $query -> fetch_assoc();
+
+  // Controllo che la nuova categoria esista
+  $sql = "SELECT * FROM categorieUtenti WHERE id = {$categoria} LIMIT 0, 1";
+  $query = $mysqli -> query($sql);
+
+  if(!$query) {
+    $console -> alert('Impossibile controllare l\'esistenza della categoria. '.$mysqli -> error, $autenticazione -> id);
+    stampaErrore();
+  }
+
+  // Controllo che la categoria esista
+  if($query -> num_rows === 0)
+    stampaErrore('Categoria inesistente!');
+
+  /*
+  Il campo codice attivazione è mappato diversamente sulla pagina web
+  Valore || Pagina web     || DB
+  0      || non verificato || verificato
+  != 0   || verificato     || non verificato
+
+  Quindi inverto il campo
+  */
+  $confermaMail = $confermaMail == 0 ? 1 : 0;
+
+  // Aggiorno i campi modificati
+  $sql = "UPDATE utenti SET ";
+
+  // Gruppo di appartenenza
+  if($utente['categoria'] != $categoria)
+    $sql .= "categoria = {$categoria}, ";
+
+  // Sospensione del profilo
+  if($utente['sospeso'] != $sospensione)
+    $sql .= "sospeso = {$sospensione}, ";
+
+  // Verifica dell'indirizzo email
+  if($utente['codiceAttivazione'] == 0 && $confermaMail === 1) {
+
+    $codiceAttivazione = uniqid();
+    $linkVerifica = $dizionario -> getValue('urlSito').'/confermaMail.php?token='.$codiceAttivazione;
+    $replyTo = ($dizionario -> getValue('EMAIL_REPLY_TO') == false || $dizionario -> getValue('EMAIL_REPLY_TO') == null) ? $dizionario -> getValue('EMAIL_SOURCE') : $dizionario -> getValue('EMAIL_REPLY_TO');
+
+    try {
+
+      // Compilo il template
+      $html = $templateManager -> getTemplate('confermaMailGestione', array(
+        'nome' => $utente['nome'],
+        'cognome' => $utente['cognome'],
+        'linkConferma' => $linkVerifica,
+        'nomeSito' => $dizionario -> getValue('nomeSito')
+      ));
+
+      // Invio l'email
+      $client -> sendEmail(array(
+        'Source' => $dizionario -> getValue('EMAIL_SOURCE'),
+        'ReplyToAddresses' => array($replyTo),
+        'Destination' => array(
+          'ToAddresses' => array($utente['email'])
+        ),
+        'Message' => array(
+          'Subject' => array(
+            'Data' => 'Verifica indirizzo email',
+            'Charset' => 'UTF-8'
+          ),
+          'Body' => array(
+            'Html' => array(
+              'Data' => $html,
+              'Charset' => 'UTF-8'
+            )
+          )
+        )
+      ));
+
+    // Errore durante l'invio dell'email
+    } catch(Exception $e) {
+      $console -> alert('Impossibile inviare l\'email di verifica. '.$e, $autenticazione -> id);
+      stampaErrore();
+    }
+
+    $sql .= "codiceAttivazione = '{$codiceAttivazione}'";
+  }
+
+  if($utente['codiceAttivazione'] != 0 && $confermaMail === 0)
+    $sql .= "codiceAttivazione = '0'";
+
+  // Se la query è diversa da quella di base
+  // La eseguo, altrimenti avviso l'utente che nulla è stato modificato
+  if($sql === "UPDATE utenti SET ")
+    stampaErrore('Nessun dato è stato modificato.');
+
+  if(mb_substr($sql, -strlen(", ")) == ", ")
+    $sql = mb_substr($sql, 0, strlen($sql)-strlen(", "));
+
+  $query = $mysqli -> query("{$sql} WHERE id = {$id}");
+
+  if(!$query) {
+    $console -> alert($sql.' Impossibile aggiornare i dati. '.$mysqli -> error, $autenticazione -> id);
+    stampaErrore();
+  }
+
+  echo '{}';
 ?>
