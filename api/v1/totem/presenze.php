8ac4d974d0176beea1948d26aba0fc70551b2eed
Edoardo Savini <sedoardo98@gmail.com>
2017-09-03 22:31:50 +0200

    Bugfix

--- api/v1/totem/presenze.php
+++ api/v1/totem/presenze.php
@@ -19,7 +19,7 @@
   else if(!preg_match("/^[0-9]{10}+$/", $rfid))
     stampaErrore('RFID non valido!');
 
-  else if(!preg_match("/^[0-9]{11}+$/", $timestamp) || (int)$timestamp < time() - 12 * 60 * 60 || (int)$timestamp > time() + 5 * 60)
+  else if(!preg_match("/^[0-9]{10,11}+$/", $timestamp) || (int)$timestamp < time() - 12 * 60 * 60 || (int)$timestamp > time() + 5 * 60)
     stampaErrore('Timestamp non valido!');
 
   // Controllo la validità del token del totem
