6bb25faf28fc21c2f30b12ee13be0d587c679374
Edoardo Savini <sedoardo98@gmail.com>
2018-01-22 18:52:40 +0100

    Aggiunta data ed ora sul server

--- inc/footer.inc.php
+++ inc/footer.inc.php
@@ -3,6 +3,7 @@
     <p>Realizzato da Savini Edoardo per FabLab Romagna.<br />Codice sorgente rilasciato con licenza LGPL-3.0 su <a href="https://github.com/98edoardo/FabLab-Romagna-Auth/" style="color: #fff">GitHub</a>.</p>
   </div>
   <div>
+    <p style="margin-bottom: 20px; text-align: center;"><b>Data e ora sul server:</b><br /><?php echo date("d/m/Y H:i:s") ?></p>
     <ul>
       <li>Mancano alcune funzioni? Proponile creando un'<a href="https://github.com/98edoardo/FabLab-Romagna-Auth/issues" style="color: #fff">issue su GitHub</a>.</li>
     </ul>
