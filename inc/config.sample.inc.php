e9f334dc263a9cf5dd976b82093dd61a27b48b7e
98edoardo <sedoardo98@gmail.com>
2018-09-21 15:49:50 +0200

    Rimosse costanti non più utilizzate.

--- inc/config.sample.inc.php
+++ inc/config.sample.inc.php
@@ -6,18 +6,8 @@ define('MYSQL_USERNAME', 'root');
 define('MYSQL_PWD', 'root');
 
 define('DEBUG', false);
-define('AUTH_COOKIE_NAME', 'lemonade_auth_cookie');
 
-// Tema
-define('TEMA_BG_PRINCIPALE', '#cecece'); // Colore sfondo home e footer e topbar
-define('TEMA_COL_TESTO_PRINCIPALE', '#151515'); // Colore del testo
-define('TEMA_BG_PRINCIPALE_LOGGED', '#2b2b2b'); // Sfondo footer e navigazione loggato
-define('TEMA_COL_TESTO_LOGGED', '#fff'); // Testo loggato footer e navigazione no link
-define('TEMA_COL_BTN_NOHOVER', '#fff'); // Testo no hover bottoni
-define('TEMA_COL_BTN_HOVER', 'rgb(0, 98, 217)'); // Testo hover bottoni
-define('TEMA_BG_BTN_NOHOVER', 'rgb(0, 98, 217)'); // Sfondo no hover bottoni
-define('TEMA_BG_BTN_HOVER', '#fff'); // Sfondo hover bottoni
-define('TEMA_COL_BTN_BORDER_DISABLED', '#cecece'); // Bordo bottoni disabilitati
+define('AUTH_COOKIE_NAME', 'lemonade_auth_cookie');
 
 // Fuso orario
 date_default_timezone_set('Europe/Rome');
