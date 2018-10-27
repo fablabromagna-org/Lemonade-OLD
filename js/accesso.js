33d8defc5e14854dae44fdcce02e6d0c9adf5b88
98edoardo <sedoardo98@gmail.com>
2018-10-27 21:56:35 +0200

    Modifiche minori

--- js/accesso.js
+++ js/accesso.js
@@ -10,46 +10,48 @@ $(document).ready(function () {
 
     accessoInCorso = true
 
+      function disabled (a = true) {
+
+          $('#email').prop('disabled', a)
+          $('#password').prop('disabled', a)
+          $('#captcha').prop('disabled', a)
+
+          if (a) {
+              $('#invioForm').hide()
+              $('.lds-ring.center').show()
+          } else {
+              $('.lds-ring.center').hide()
+              $('#invioForm').show()
+          }
+
+      }
+
     var email = $('#email')
     var pwd = $('#password')
     var captcha = $('#captcha')
-    $('#invioForm').prop('disabled', true)
+      disabled()
 
     if (email.val() === '') {
       email.effect('shake')
       accessoInCorso = false
-      $('#invioForm').prop('disabled', false)
+        disabled(false)
       return
     }
 
     if ((pwd.val()).length < 6) {
       pwd.effect('shake')
       accessoInCorso = false
-      $('#invioForm').prop('disabled', false)
-
-      return
-    }
-
-    if (!boxCaptcha) {
-      $('#credenziali').hide()
-      $('#captchaBox').show()
-      captcha.focus()
-      accessoInCorso = false
-      boxCaptcha = true
-      $('#invioForm').val('Accedi')
-      $('#invioForm').prop('disabled', false)
+        disabled(false)
       return
     }
 
     if (captcha.val() === '') {
       captcha.effect('shake')
       accessoInCorso = false
-      $('#invioForm').prop('disabled', false)
+        disabled(false)
       return
     }
-
-    $('#captchaBox').hide()
-    $('#spinnerBox').show()
+      $('#caricamento').show()
 
     $.ajax('/ajax/accesso.php', {
       method: 'POST'
@@ -61,51 +63,45 @@ $(document).ready(function () {
       error: function (data) {
         data = JSON.parse(data.responseText)
 
-        setTimeout(function () {
           var field = data.field
 
           if (field !== undefined) {
 
-            $('#spinnerBox').hide()
+              disabled(false)
 
-            if (field === 'email') {
-              $('#credenziali').show()
-              email.effect('shake')
-              $('#invioForm').val('Avanti')
-            }
+              if (field === 'email') {
+                  $('#credenziali').show()
+                  email.effect('shake')
+                  $('#invioForm').val('Avanti')
+              }
 
-            if (field === 'password') {
-              $('#credenziali').show()
-              pwd.effect('shake')
-              $('#invioForm').val('Avanti')
-            }
+              if (field === 'password') {
+                  $('#credenziali').show()
+                  pwd.effect('shake')
+                  $('#invioForm').val('Avanti')
+              }
 
-            if (field === 'captcha') {
-              $('#captchaBox').show()
-              captcha.effect('shake')
-            }
+              if (field === 'captcha') {
+                  $('#captchaBox').show()
+                  captcha.effect('shake')
+              }
 
-            accessoInCorso = false
-            boxCaptcha = false
-            $('#invioForm').prop('disabled', false)
+              accessoInCorso = false
+              boxCaptcha = false
 
           } else {
-            alert(data.alert)
-            accessoInCorso = false
-            boxCaptcha = false
-            $('#spinnerBox').hide()
-            $('#credenziali').show()
-            $('#invioForm').val('Avanti')
-            $('#invioForm').prop('disabled', false)
+              alert(data.alert)
+              accessoInCorso = false
+              boxCaptcha = false
+              disabled(false)
 
           }
 
           if (data.refreshCaptcha === true) {
-            $('#captchaImg').prop('src', '/ajax/captcha.php')
-            captcha.val('')
+              $('#captchaImg').prop('src', '/ajax/captcha.php')
+              captcha.val('')
           }
 
-        }, 1500)
       }
       , data: {
         'email': email.val()
